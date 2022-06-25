8722674761fd1fbd6a020cb0501d79b5764509d9
Doriane77 <dlolliadu77@hotmail.fr>
2022-06-25 20:26:00 +0200

    update room

--- Routes/room.js
+++ Routes/room.js
@@ -3,8 +3,29 @@ const router = express.Router();
 const db = require("../databaseConfig");
 
 router.post("/room/publish", (req, res) => {
-  console.log("poster une offre");
-  res.json({ message: "poster une offre" });
+  try {
+    const { title, description, price, lat, lng, photo, user } = req.fields;
+    console.log(
+      "title :",
+      title,
+      "description :",
+      description,
+      "price :",
+      price,
+      "lat :",
+      lat,
+      "lng :",
+      lng,
+      "photo :",
+      photo,
+      "user :",
+      user
+    );
+    console.log("poster une offre");
+    res.json({ message: "poster une offre" });
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
 module.exports = router;
