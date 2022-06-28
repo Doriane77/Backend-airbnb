7e7576d3c6ef5647053fbafab09378322310839b
Doriane77 <dlolliadu77@hotmail.fr>
2022-06-28 17:58:13 +0200

    update room route

--- Routes/room.js
+++ Routes/room.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../databaseConfig");
+const isAuthenticated = require("../Middlewares/isAuthenticated");
 
-router.post("/room/publish", (req, res) => {
+router.get("/rooms", isAuthenticated, (req, res) => {
+  try {
+    console.log(req.fields);
+    res.json({ message: "rooms" });
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+router.post("/room/publish", isAuthenticated, (req, res) => {
   try {
     const { title, description, price, lat, lng, photo, user } = req.fields;
     console.log(
