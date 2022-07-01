e4138382cfa91c5d56a29ec0b0777f2485cebd5f
Doriane77 <dlolliadu77@hotmail.fr>
2022-07-01 16:46:15 +0200

    database and cloudinary config

---
+++ Config/cloudinaryConfig.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRET_KEY,
+});
+
+module.exports = cloudinary;
