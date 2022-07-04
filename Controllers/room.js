97ddfade82609c60aae2368a8909f3d59f397668
Doriane77 <dlolliadu77@hotmail.fr>
2022-07-04 22:52:51 +0200

    room pusblish offer

--- Controllers/room.js
+++ Controllers/room.js
@@ -7,14 +7,11 @@ const allRoom = (req, res) => {
       if (err) {
         res.json(err);
       } else {
-        console.log(result);
         const resulta = result;
-        const tab = result[0].photo;
-        resulta[0].photo = Buffer.from(tab);
-        // console.log("RESULTA :", resulta[0].photo.toJSON());
-
-        // console.log("result :", result[0].photo.toJSON());
-        console.log(resulta);
+        for (let i = 0; i < resulta.length; i++) {
+          const obj = result[i].photo;
+          resulta[i].photo = JSON.parse(obj);
+        }
         res.json(resulta);
       }
     });
@@ -27,32 +24,16 @@ const publishRoom = async (req, res) => {
   try {
     const { title, description, price, lat, lng, userId } = req.fields;
 
-    console.log(
-      "title :",
-      title,
-      "description :",
-      description,
-      "price :",
-      price,
-      "lat :",
-      lat,
-      "lng :",
-      lng
-    );
     const photo = req.files.photo.path;
 
-    console.log("PHOTO ====>", photo);
-
-    const picture = await cloudinary.uploader.upload(photo, {
-      folder: `/Airbnb/rooms/`,
-    });
-    console.log("type de picture :", typeof picture);
-
     if (title && description && price && lat && lng && photo) {
-      console.log("VALIDER");
+      const picture = await cloudinary.uploader.upload(photo, {
+        folder: `/Airbnb/rooms/`,
+      });
 
+      const convertPicture = JSON.stringify(picture);
       db.query(
-        `INSERT INTO room SET title="${title}", description="${description}", price="${price}", lat="${lat}", lng="${lng}", user="${userId}", photo="${picture}"`,
+        `INSERT INTO room SET title="${title}", description="${description}", price="${price}", lat="${lat}", lng="${lng}", user="${userId}", photo='${convertPicture}'`,
         (err, result) => {
           if (err) {
             res.json(err);
@@ -66,35 +47,13 @@ const publishRoom = async (req, res) => {
                 if (err) {
                   res.json(err);
                 } else {
-                  console.log("SELECT OK");
-                  console.log("result :", result);
-
-                  res.json(result);
-
-                  /*  const picture = await cloudinary.uploader.upload(photo, {
-                    folder: `/Airbnb/rooms/${result.id}`,
-                  });
-                  console.log(picture);
-                  db.query(
-                    `UPDATE room SET photo=${picture} WHERE id=${result.id} `,
-                    (err, result) => {
-                      if (err) {
-                        res.json(err);
-                      } else {
-                        db.query(
-                          `SELECT id,title,description,price,lat,lng,photo,user FROM room WHERE id=${id}"`,
-                          (err, result) => {
-                            res.json(result);
-                          }
-                        );
-                      }
-                    }
-                  );
-                  res.json({ message: result });*/
+                  const resulta = result;
+                  const obj = result[0].photo;
+                  resulta[0].photo = JSON.parse(obj);
+                  res.json(resulta);
                 }
               }
             );
-            // res.json({ message: "poster une offre" });
           }
         }
       );
@@ -106,4 +65,13 @@ const publishRoom = async (req, res) => {
   }
 };
 
-module.exports = { allRoom, publishRoom };
+const deleteRoom = (req, res) => {
+  try {
+    const room = req.fields.room;
+    console.log("supprimer ,", room);
+    res.json({ message: "Room supprimer." });
+  } catch (error) {
+    res.json(error);
+  }
+};
+module.exports = { allRoom, publishRoom, deleteRoom };
