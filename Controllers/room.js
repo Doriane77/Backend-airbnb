3f22d231ebbc69a8a10a35b3018597891d32e347
Doriane77 <dlolliadu77@hotmail.fr>
2022-07-05 17:40:42 +0200

    update room

--- Controllers/room.js
+++ Controllers/room.js
@@ -67,11 +67,141 @@ const publishRoom = async (req, res) => {
 
 const deleteRoom = (req, res) => {
   try {
-    const room = req.fields.room;
-    console.log("supprimer ,", room);
-    res.json({ message: "Room supprimer." });
+    const roomId = req.fields.roomId;
+    if (roomId) {
+      db.query(
+        `SELECT * FROM room WHERE id="${roomId}"`,
+        async (err, result) => {
+          if (err) {
+            res.json(err);
+          } else {
+            const resulta = result;
+            const obj = result[0].photo;
+            resulta[0].photo = JSON.parse(obj);
+            const pictureId = resulta[0].photo.public_id;
+
+            const picture = await cloudinary.uploader.destroy(pictureId);
+
+            if (picture.result === "ok") {
+              db.query(`DELETE FROM room WHERE id=${roomId}`, (err, result) => {
+                if (err) {
+                  res.json(err);
+                } else {
+                  console.log(result);
+                  res.json({ message: "Room deleted" });
+                }
+              });
+            }
+          }
+        }
+      );
+    } else {
+      res.json({ message: "Missing roomId" });
+    }
   } catch (error) {
     res.json(error);
   }
 };
-module.exports = { allRoom, publishRoom, deleteRoom };
+
+const updateRoom = (req, res) => {
+  try {
+    const { roomId, title, description, price, lat, lng } = req.fields;
+    const photo = req.files.photo.path;
+
+    if (roomId) {
+      if (title) {
+        db.query(
+          `UPDATE room SET title="${title}" WHERE id=${roomId}`,
+          (err, result) => {
+            if (err) {
+              res.json(err);
+            } else {
+              console.log("title updade");
+            }
+          }
+        );
+      }
+      if (description) {
+        db.query(
+          `UPDATE room SET description="${description}" WHERE id=${roomId}`,
+          (err, result) => {
+            if (err) {
+              res.json(err);
+            } else {
+              console.log("description update");
+            }
+          }
+        );
+      }
+      if (price) {
+        db.query(
+          `UPDATE room SET price="${price}" WHERE id=${roomId}`,
+          (err, result) => {
+            if (err) {
+              res.json(err);
+            } else {
+              console.log("prise update");
+            }
+          }
+        );
+      }
+      if (lat && lng) {
+        db.query(``, (err, result) => {
+          if (err) {
+            res.json(err);
+          } else {
+            console.log("lat and lng update");
+          }
+        });
+      }
+      if (req.files.photo.name != "") {
+        db.query(
+          `SELECT * FROM room WHERE id=${roomId}`,
+          async (err, result) => {
+            if (err) {
+              res.json(err);
+            } else {
+              const obj = result[0].photo;
+
+              const resulta = JSON.parse(obj);
+
+              const pictureId = resulta.public_id;
+
+              const deletePicture = await cloudinary.uploader.destroy(
+                pictureId
+              );
+
+              if (deletePicture.result === "ok") {
+                const picture = await cloudinary.uploader.upload(photo, {
+                  folder: `/Airbnb/rooms/`,
+                });
+
+                const convertPicture = JSON.stringify(picture);
+
+                db.query(
+                  `UPDATE room SET photo='${convertPicture}' WHERE id =${roomId}`,
+                  (err, result) => {
+                    if (err) {
+                      res.json(err);
+                    } else {
+                      console.log("update photo :", result);
+                      console.log("photo update");
+                      res.json({ message: "Update room and picture" });
+                    }
+                  }
+                );
+              }
+            }
+          }
+        );
+      } else {
+        res.json({ message: "Update room" });
+      }
+    } else {
+      res.json({ message: "Missing roomId" });
+    }
+  } catch (error) {
+    res.json(error);
+  }
+};
+module.exports = { allRoom, publishRoom, deleteRoom, updateRoom };
