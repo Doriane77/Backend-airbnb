cec090ab60b33670b984cb219d3b4779d27eefd2
Doriane77 <dlolliadu77@hotmail.fr>
2022-07-09 23:43:38 +0200

    order all rooms

--- Controllers/room.js
+++ Controllers/room.js
@@ -1,12 +1,55 @@
 const db = require("../Config/databaseConfig");
 const cloudinary = require("../Config/cloudinaryConfig");
 const { unsubscribe } = require("../Routes/user");
+const { image } = require("../Config/cloudinaryConfig");
 
 const allRoom = (req, res) => {
   try {
     const { title, maxPrice, minPrice } = req.query;
 
-    db.query(`SELECT * FROM room`, async (err, result) => {
+    let page = 1;
+    let limit;
+    if (Number(req.query.page) > 0) {
+      page = Number(req.query.page);
+    } else {
+      page = 1;
+    }
+    if (Number(req.query.limit) > 0) {
+      limit = Number(req.query.limit);
+    } else {
+      limit = 20;
+    }
+    console.log("page :", page);
+    console.log("limit :", limit);
+    console.log("maxPrice :", maxPrice);
+
+    let sql = `Select * FROM room ORDER BY price DESC`;
+
+    //
+
+    if (title || maxPrice || minPrice) {
+      console.log("change sql");
+      if (title && maxPrice === undefined && minPrice === undefined) {
+        console.log("title");
+        sql = `SELECT * FROM room WHERE title LIKE '%${title}%' ORDER BY price DESC LIMIT ${page},${limit}`;
+      }
+      if (maxPrice && title === undefined && minPrice === undefined) {
+        console.log("maxPrice");
+        sql = `SELECT * FROM room WHERE price <= ${maxPrice}  ORDER BY price DESC LIMIT ${page},${limit}`;
+      }
+      if (minPrice && title === undefined && maxPrice === undefined) {
+        console.log("minPrice");
+        sql = `SELECT * FROM room WHERE price >= ${minPrice} ORDER BY price LIMIT ${page},${limit}`;
+      }
+      if (title && maxPrice && minPrice === undefined) {
+        console.log("title and maxPrice");
+        sql = `SELECT * FROM room WHERE title LIKE '%${title}%' AND price<${maxPrice} ORDER BY price DESC LIMIT ${page},${limit}`;
+      }
+    }
+
+    //
+    console.log("sql :", sql);
+    db.query(sql, async (err, result) => {
       if (err) {
         res.json(err);
       } else {
@@ -16,40 +59,59 @@ const allRoom = (req, res) => {
         for (let i = 0; i < resulta.length; i++) {
           const obj = result[i].photo;
           resulta[i].photo = JSON.parse(obj);
-
-          if (title || maxPrice || minPrice) {
-            if (
-              resulta[i].title.includes(title) &&
-              resulta[i].price <= +maxPrice &&
-              resulta[i].price >= +minPrice
-            ) {
-              tab.push(resulta[i]);
-            }
-            if (
-              (resulta[i].title.includes(title) &&
-                resulta[i].price >= +minPrice) ||
-              (resulta[i].title.includes(title) &&
-                resulta[i].price <= +maxPrice) ||
-              (resulta[i].price <= +maxPrice && resulta[i].price >= +minPrice)
-            ) {
-              tab.push(resulta[i]);
-            }
-            if (
-              (title && maxPrice === undefined && minPrice === undefined) ||
-              (maxPrice && title === undefined && minPrice === undefined) ||
-              (minPrice && title === undefined && maxPrice === undefined)
-            ) {
+          /*
+            if (title || maxPrice || minPrice) {
               if (
-                resulta[i].title.includes(title) ||
-                resulta[i].price <= +maxPrice ||
+                resulta[i].title.includes(title) &&
+                resulta[i].price <= +maxPrice &&
                 resulta[i].price >= +minPrice
               ) {
                 tab.push(resulta[i]);
               }
-            }
-          }
+              if (
+                (resulta[i].title.includes(title) &&
+                  resulta[i].price >= +minPrice) ||
+                (resulta[i].title.includes(title) &&
+                  resulta[i].price <= +maxPrice) ||
+                (resulta[i].price <= +maxPrice && resulta[i].price >= +minPrice)
+              ) {
+                tab.push(resulta[i]);
+              }
+              if (
+                (title && maxPrice === undefined && minPrice === undefined) ||
+                (maxPrice && title === undefined && minPrice === undefined) ||
+                (minPrice && title === undefined && maxPrice === undefined)
+              ) {
+                if (
+                  resulta[i].title.includes(title) ||
+                  resulta[i].price <= +maxPrice ||
+                  resulta[i].price >= +minPrice
+                ) {
+                  tab.push(resulta[i]);
+                }
+              }
+            }*/
+          //  console.log("RESULTA ===> ", resulta[i].price);
         }
+        return res.json(resulta);
         if (title || maxPrice || minPrice) {
+          /* let page = 1;
+            let limit;
+            if (Number(req.query.page) > 0) {
+              page = Number(req.query.page);
+            } else {
+              page = 1;
+            }
+            if (Number(req.query.limit) > 0) {
+              limit = Number(req.query.limit);
+            } else {
+              limit = 10;
+            }
+            console.log("page :", page);
+            console.log("limit :", limit);
+            const counter = Math.ceil(tab.length / limit);
+            console.log(counter);
+*/
           return res.json(tab);
         } else {
           res.json(resulta);
