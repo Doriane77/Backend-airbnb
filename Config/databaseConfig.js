ceebe17349cb4ecb8c18ae4e56d37239acd3ebc5
Doriane77 <dlolliadu77@hotmail.fr>
2022-07-12 16:40:04 +0200

    new information connection

--- Config/databaseConfig.js
+++ Config/databaseConfig.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "tuto",
+  host: process.env.SQL_HOST,
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  database: process.env.SQL_DATABASE,
 });
 // database: "airbnb",
 db.connect(function (err) {
