1a523c00c2f1fe22fa916c378723978daee5539a
Doriane77 <dlolliadu77@hotmail.fr>
2022-08-24 19:34:07 +0200

    route arrond me

--- Routes/room.js
+++ Routes/room.js
@@ -9,6 +9,7 @@ const controller = require("../Controllers/room");
 
 router.get("/room/:id", controller.oneRoom);
 router.get("/rooms", controller.allRoom);
+router.get("/rooms/arround", controller.roomAround);
 router.get("/rooms/user/:id", controller.roomsUser);
 router.post("/room/publish", isAuthenticated, controller.publishRoom);
 router.post("/room/delete", isAuthenticated, controller.deleteRoom);
